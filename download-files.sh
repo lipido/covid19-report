eaca368bed4e3572ab3bf7225712f1a2d8a97f12
lipido <dgpena@uvigo.es>
2020-04-13 16:45:54 +0200

    Adds new data and an initial script for stacked area charts of epidemy evolution

---
+++ download-files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv" -O time_series_covid19_confirmed_global.csv
+
+wget "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv&filename=time_series_covid19_deaths_global.csv" -O time_series_covid19_deaths_global.csv
+
+wget "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_recovered_global.csv&filename=time_series_covid19_recovered_global.csv" -O time_series_covid19_recovered_global.csv
---
+++ stacked-charts.R
@@ -0,0 +1,35 @@
+library(ggplot2)
+library(dplyr)
+
+covid19_stacked_chart <- function(country="worldwide") {  
+  data_confirmed <- read.csv("time_series_covid19_confirmed_global.csv", head=TRUE)
+  data_recovered <- read.csv("time_series_covid19_recovered_global.csv", head=TRUE)
+  data_deaths <- read.csv("time_series_covid19_deaths_global.csv", head=TRUE)
+
+  
+
+  if (country == "worldwide") {
+    #worldwide
+    confirmed <- as.numeric(colSums(data_confirmed[,5:ncol(data_confirmed)]))
+    recovered<- as.numeric(colSums(data_recovered[,5:ncol(data_recovered)]))
+    deaths <- as.numeric(colSums(data_deaths[,5:ncol(data_deaths)]))
+  } else {
+    
+    confirmed <- as.numeric(colSums(data_confirmed[data_confirmed[,"Country.Region"]==country,5:ncol(data_confirmed)]))
+    recovered<- as.numeric(colSums(data_recovered[data_recovered[,"Country.Region"]==country,5:ncol(data_recovered)]))
+    deaths <- as.numeric(colSums(data_deaths[data_deaths[,"Country.Region"]==country,5:ncol(data_deaths)]))
+    
+    
+  }
+
+
+
+  active <- confirmed-(recovered + deaths)
+
+  time <- rep(seq(1:length(recovered)), 3)
+  value <- c(recovered, deaths, active)
+  group <- rep(c("recovered","deaths", "active"), each=length(confirmed))
+  group <- factor(group, levels=c("recovered","deaths", "active"))
+  data <- data.frame(time, value, group)
+  ggplot(data, aes(x=time, y=value, fill=group)) + geom_area() + ggtitle(paste("covid19 evolution in ", country)) + theme(legend.position="bottom")
+}
